45015ef06f296d07eb9238c386889ad418b7afaa
rykn0wxx <raurici666@gmail.com>
2018-07-03 21:04:07 -0400

    base commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :dim_regions
+    resources :dim_timezones
+    root :to => 'dim_regions#index'
+  end
+  get 'index' => 'explorer#index', :as => 'index'
+  root :to => 'explorer#index'
 end
